d5cacf7edccf10ec3d06d3848efa1edcb88f525c
VenkataSaiSabbineni <venkatasaisabbineni@gmail.com>
2024-07-22 14:34:19 -0400

    Updated Jenkinsfile

--- etl/drivers.py
+++ etl/drivers.py
@@ -27,25 +27,25 @@ type_of_method = DRIVERS.split('/')[-1]
 file_path = f'{OUTPUT_PATH}/{type_of_method}'
 df.write.option("header",True).partitionBy("meeting_key","session_key").csv(file_path)
 #load
-try:
-    s3 = s3 = s3fs.S3FileSystem(anon=False,
-                               key= AWS_ACCESS_KEY_ID,
-                               secret=AWS_ACCESS_KEY)
-except Exception as e:
-    print(e)
-# #check if the bucket is there or not
 # try:
-#         if not s3.exists(AWS_BUCKET_NAME):
-#             s3.mkdir(AWS_BUCKET_NAME)
-#             print("Bucket created")
-#         else :
-#             print("Bucket already exists")
+#     s3 = s3 = s3fs.S3FileSystem(anon=False,
+#                                key= AWS_ACCESS_KEY_ID,
+#                                secret=AWS_ACCESS_KEY)
 # except Exception as e:
 #     print(e)
-#upload to the bucket
-try:
-        s3.put(file_path, AWS_BUCKET_NAME,recursive=True)
-        print('File uploaded to s3')
-except FileNotFoundError:
-    print('The file was not found')
+# # #check if the bucket is there or not
+# # try:
+# #         if not s3.exists(AWS_BUCKET_NAME):
+# #             s3.mkdir(AWS_BUCKET_NAME)
+# #             print("Bucket created")
+# #         else :
+# #             print("Bucket already exists")
+# # except Exception as e:
+# #     print(e)
+# #upload to the bucket
+# try:
+#         s3.put(file_path, AWS_BUCKET_NAME,recursive=True)
+#         print('File uploaded to s3')
+# except FileNotFoundError:
+#     print('The file was not found')
 spark.stop()
