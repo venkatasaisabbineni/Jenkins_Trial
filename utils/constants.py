2188fe8c0d51613ac5011e18be8dc7f88616a9c9
VenkataSaiSabbineni <venkatasaisabbineni@gmail.com>
2024-07-22 14:36:04 -0400

    Updated constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -7,11 +7,11 @@ parser.read(os.path.join(os.path.dirname(__file__), '../config/config.conf'))
 #Data
 OUTPUT_PATH = parser.get('data','output_path')
 
-#AWS
-AWS_ACCESS_KEY_ID = parser.get('aws', 'aws_access_key_id')
-AWS_ACCESS_KEY = parser.get('aws', 'aws_secret_access_key')
-AWS_REGION = parser.get('aws', 'aws_region')
-AWS_BUCKET_NAME = parser.get('aws', 'aws_bucket_name')
+# #AWS
+# AWS_ACCESS_KEY_ID = parser.get('aws', 'aws_access_key_id')
+# AWS_ACCESS_KEY = parser.get('aws', 'aws_secret_access_key')
+# AWS_REGION = parser.get('aws', 'aws_region')
+# AWS_BUCKET_NAME = parser.get('aws', 'aws_bucket_name')
 
 #API
 DRIVERS = parser.get('api_methods', 'drivers')
